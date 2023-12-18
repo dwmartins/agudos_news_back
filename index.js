bd5813115be8cc3c9e7f7052dc23fe2be08c90c8
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2023-12-17 22:42:06 -0300

    fix: Correção para verificar a porta do servidor

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const port = process.env.SERVER_PORT;
 
 try {
     if(!port || !process.env) {
-        logger.log(`error`, `O arquivo .env não existe ou uma porta para o servidor não foi utilizada.`);
+        logger.log(`error`, `O arquivo (.env) não existe ou uma porta para o servidor não foi setada.`);
     } else {
         app.listen(port, () => {
             console.log(`Servidor em execução na porta: ${port}`);
         });
     }
 } catch (error) {
-    logger.log(`Erro durante inicialização do servidor: ${error}`);
+    logger.log(`error`,`Erro durante inicialização do servidor: ${error}`);
 }
