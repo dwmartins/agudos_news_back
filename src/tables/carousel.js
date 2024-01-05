dda3af40b5c282aabb3d2fbc6b0934da17de0cc1
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2024-01-05 18:54:09 -0300

    feat: Criando tablemas de carousel e carousel_price

--- src/tables/carousel.js
+++ src/tables/carousel.js
@@ -3,6 +3,26 @@ const logger = require("../../config/logger");
 const helper = require("../utilities/helper");
 
 class NewTableCarousel {
+
+    carouselPrice = async () => {
+        try {
+            await db.pool.query(`
+                CREATE TABLE IF NOT EXISTS carousel_price (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    description VARCHAR(50) NOT NULL,
+                    price DECIMAL(10,2) NOT NULL,
+                    active ENUM('Y', 'N') DEFAULT 'Y',
+                    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                );
+            `);
+
+        } catch (error) {
+            console.log('kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk')
+            logger.log(`error`, `Falha ao criar a tabela (carousel_price): ${error}`);
+        }
+    }
+
     carousel = async () => {
         try {
             await db.pool.query(`
@@ -11,10 +31,14 @@ class NewTableCarousel {
                     user_id INT NOT NULL,
                     description VARCHAR(50),
                     status VARCHAR(50) NOT NULL,
+                    carousel_type INT NOT NULL,
+                    payment DECIMAL(10,2) NOT NULL,
+                    payment_type VARCHAR(50) NOT NULL,
                     image LONGTEXT NOT NULL,
                     createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
                     updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+                    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+                    FOREIGN KEY (carousel_type) REFERENCES carousel_price(id) ON DELETE CASCADE
                 );
             `);
         } catch (error) {
@@ -23,6 +47,8 @@ class NewTableCarousel {
     }
 
     createAll = async () => {
+        console.log(`${helper.getDateTime()} - Criando tabela de (carousel_price)...`)
+        await this.carouselPrice();
         console.log(`${helper.getDateTime()} - Criando tabela de (carousel)...`)
         await this.carousel();
     }
