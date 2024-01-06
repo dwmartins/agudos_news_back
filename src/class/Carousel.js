01d137ade0d8b0e09b0e2a41b873abce1edc425c
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2024-01-06 15:17:10 -0300

    feat: Ataulizando a class de Carousel

--- src/class/Carousel.js
+++ src/class/Carousel.js
@@ -6,6 +6,9 @@ class Carousel {
         this.user_id        = carousel.user_id;
         this.description    = carousel.description;
         this.status         = carousel.status;
+        this.carousel_type  = carousel.carousel_type;
+        this.payment        = carousel.payment;
+        this.payment_type   = carousel.payment_type;
         this.image          = carousel.image;
         this.createdAt      = carousel.createdAt;
         this.updatedAt      = carousel.updatedAt;
@@ -37,6 +40,30 @@ class Carousel {
 
     setStatus = (status) => {
         this.status = status;
+    }
+
+    getCarouselType = () => {
+        return this.carousel_type;
+    }
+
+    setCarouselType = (carousel_type) => {
+        this.carousel_type = carousel_type;
+    }
+
+    getPayment = () => {
+        return this.payment;
+    }
+
+    setPayment = (payment) => {
+        this.payment = payment;
+    }
+
+    getPaymentType = () => {
+        return this.payment_type;
+    }
+
+    setPaymentType = (payment_type) => {
+        this.payment_type = payment_type;
     }
 
     getImage = () => {
