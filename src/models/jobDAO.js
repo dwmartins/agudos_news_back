d81d9a5feb6d814cac4d951a7b4c944a359c6296
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2024-01-11 19:16:25 -0300

    feat: Criando a model para os jobs

---
+++ src/models/jobDAO.js
@@ -0,0 +1,87 @@
+const db = require("../../config/dbConnection");
+const logger = require("../../config/logger");
+
+class JobDAO {
+    constructor () {
+        this.conn = db.pool;
+    }
+
+    saveDAO = async (jobs) => {
+        const fields = Object.keys(jobs).join(', ');
+        const placeholders = Object.keys(jobs).map(key => `?`).join(', ');
+
+        const sql = `INSERT INTO jobs (${fields}) VALUES (${placeholders})`;
+        const values = Object.values(jobs);
+
+        try {
+            return await this.conn.query(sql, values);
+        } catch (error) {
+            logger.log(`error`,`Erro ao salvar a vaga de emprego: ${error}`);
+            throw error
+        } 
+    }
+
+    updateDAO = async (jobs) => {
+        const jobsId = jobs.id;
+        delete jobs.id;
+        const fields = Object.keys(jobs).join(' = ?, ') + ' = ?';
+        
+        const sql = `UPDATE jobs SET ${fields} WHERE id = ?`;
+        let values = [...Object.values(jobs), jobsId];
+
+        try {
+            await this.conn.query(sql, values);
+            return true;
+        } catch (error) {
+            logger.log(`error`,`Falha ao atualizar a vaga de emprego: ${error}`);
+            throw error
+        }
+    }
+
+    deleteDAO = async (id) => { 
+        try {
+            const sql = `DELETE FROM jobs WHERE id = ?`;
+            const value = [id];
+
+            await this.conn.query(sql, value);
+            return true;
+        } catch (error) {
+            logger.log(`error`, `Falha ao deletar a vaga de emprego: ${error}`);
+            throw error
+        }
+    }
+
+    findById = async (id) => {
+        try {
+            const sql = `SELECT id FROM jobs WHERE id = ?`;
+            const value = [id];
+
+            const result = this.conn.query(sql, value);
+
+            return result[0];
+        } catch (error) {
+            logger.log(`error`, `Falha ao buscar as vagas de emprego por id: ${error}`);
+            throw error
+        }
+    }
+
+    findAll = async (status = null) => {
+        let sql = `SELECT * FROM jobs`;
+
+        if(status) {
+            sql += ` WHERE status = ?`;
+        }
+        
+        const value = [status];
+
+        try {
+            const result = await this.conn.query(sql, value);
+            return result[0];
+        } catch (error) {
+            logger.log(`error`, `Falha ao buscas as vagas de emprego: ${error}`);
+            throw error
+        }
+    }
+}
+
+module.exports = new JobDAO;
