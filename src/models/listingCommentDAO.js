f6140a1485f9c049364c872c8599999eab99200a
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2024-01-23 23:18:07 -0300

    feat: Criando a model para os comentarios

---
+++ src/models/listingCommentDAO.js
@@ -0,0 +1,97 @@
+const db = require("../../config/dbConnection");
+const logger = require("../../config/logger");
+
+class ListingCommentDAO {
+    constructor () {
+        this.conn = db.pool;
+    }
+
+    saveDAO = async (comment) => {
+        const fields = Object.keys(comment).join(', ');
+        const placeholders = Object.keys(comment).map(key => `?`).join(', ');
+
+        const sql = `INSERT INTO listing_comment (${fields}) VALUES (${placeholders})`;
+        const values = Object.values(comment);
+
+        try {
+            const result = await this.conn.query(sql, values);
+            return result[0];
+        } catch (error) {
+            logger.log(`error`,`Erro ao salvar a comentário da listing: ${error}`);
+            throw new Error(error);
+        } 
+    }
+
+    updateDAO = async (comment) => {
+        const commentId = comment.id;
+        delete comment.id;
+        const fields = Object.keys(comment).join(' = ?, ') + ' = ?';
+        
+        const sql = `UPDATE listing_comment SET ${fields} WHERE id = ?`;
+        let values = [...Object.values(comment), commentId];
+
+        try {
+            await this.conn.query(sql, values);
+            return true;
+        } catch (error) {
+            logger.log(`error`,`Falha ao atualizar o comentário da listing: ${error}`);
+            throw new Error(error);
+        }
+    }
+
+    deleteDAO = async (id) => { 
+        try {
+            const sql = `DELETE FROM listing_comment WHERE id = ?`;
+            const value = [id];
+
+            await this.conn.query(sql, value);
+            return true;
+        } catch (error) {
+            logger.log(`error`, `Falha ao deletar o comentário da listing: ${error}`);
+            throw new Error(error);
+        }
+    }
+
+    findByListing = async (listingId) => {
+        try {
+            const sql = `
+                SELECT listing_comment.*, user.name as userName
+                FROM listing_comment
+                INNER JOIN users ON listing_comment.user = user.id
+                WHERE listing_comment.id = ?`;
+
+            const values = [listingId];
+
+            const results = this.conn.query(sql, values);
+            return results[0];
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+    findById = async (id) => {
+        try {
+            const sql = `SELECT id FROM listing_comment WHERE id = ?`;
+            const value = [id];
+
+            const result = this.conn.query(sql, value);
+
+            return result[0];
+        } catch (error) {
+            logger.log(`error`, `Falha ao buscar os comentários da listing por id: ${error}`);
+            throw new Error(error);
+        }
+    }
+
+    findAll = async () => {
+        const sql = `SELECT * FROM listing_comment`;
+
+        try {
+            const result = await this.conn.query(sql);
+            return result[0];
+        } catch (error) {
+            logger.log(`error`, `Falha ao buscas os comentários da listing: ${error}`);
+            throw new Error(error);
+        }
+    }
+}
