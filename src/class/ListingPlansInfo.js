adad63f4d0fc28fc1d21fecc45c3a038726b6bbc
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2024-02-05 19:17:58 -0300

    Criando class para ListingPlansInfo

---
+++ src/class/ListingPlansInfo.js
@@ -0,0 +1,47 @@
+class ListingPlansInfo {
+    constructor(plan) {
+        this.id             = plan.id;
+        this.plansId        = plan.plansId;
+        this.active         = plan.active;
+        this.description    = plan.description;
+        this.createdAt      = plan.createdAt;
+        this.updatedAt      = plan.updatedAt;
+    }
+
+    getId= () => this.id;
+
+    getPlansId = () => this.plansId;
+    setPlansId = (plan) => this.plansId = plan;
+
+    getActive = () => this.active;
+    setActive = (active) => this.active = active;
+
+    getDescription = () => this.description;
+    setDescription = (description) => this.price = description;
+
+    getCreateAt = () => this.createdAt;
+    getUpdatedAt = () =>  this.updatedAt;
+
+    save = async () => {
+        let plainObject = Object.fromEntries(
+            Object.entries(this).filter(([key, value]) => typeof value !== 'function')
+        );
+
+        delete plainObject.createdAt;
+        delete plainObject.updatedAt;
+        delete plainObject.id;
+
+        return await listingPriceDAO.saveDAO(plainObject);
+    }
+
+    update = async () => {
+        let plainObject = Object.fromEntries(
+            Object.entries(this).filter(([key, value]) => typeof value !== 'function')
+        );
+
+        delete plainObject.createdAt;
+        delete plainObject.updatedAt;
+
+        return await listingPriceDAO.updateDAO(plainObject);
+    }
+}
