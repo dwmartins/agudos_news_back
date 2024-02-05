8cbf3a54dcc00723f0db72806c6dda096b2fdb60
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2024-02-05 19:23:50 -0300

    Criando a model para listingPlansInfo

---
+++ src/models/listingPlansInfoDAO.js
@@ -0,0 +1,57 @@
+const db = require("../../config/dbConnection");
+const logger = require("../../config/logger");
+
+class ListingPlansDAO {
+    constructor () {
+        this.conn = db.pool;
+    }
+
+    saveDAO =  async (plans) => {
+        const fields = Object.keys(plans).join(', ');
+        const placeholders = Object.keys(plans).map(key => `?`).join(', ');
+
+        const sql = `INSERT INTO listing_plans_info (${fields}) VALUES (${placeholders})`;
+        const values = Object.values(plans);
+
+        try {
+            await this.conn.query(sql, values);
+            return true;
+        } catch (error) {
+            logger.log(`error`,`Falha ao inserir as informações do plano: ${error}`);
+            throw new Error(error);
+        }
+    }
+
+    updateDAO = async (plans) => {
+        const plansId = plans.id;
+        delete plans.id;
+        const fields = Object.keys(plans).join(' = ?, ') + ' = ?';
+        
+        const sql = `UPDATE listing_plans_info SET ${fields} WHERE id = ?`;
+        let values = [...Object.values(plans), plansId];
+
+        try {
+            await this.conn.query(sql, values);
+            return true;
+        } catch (error) {
+            logger.log(`error`,`Falha ao atualizar as informações do plano: ${error}`);
+            throw new Error(error);
+        }
+    }
+
+    findByPlanId = async (id) => {
+        try {
+            const sql = `SELECT id FROM listing_plans_info WHERE plansId = ?`;
+            const value = [id];
+
+            const result = await this.conn.query(sql, value);
+
+            return result[0];
+        } catch (error) {
+            logger.log(`error`, `Falha ao buscar o preço do da listing por id: ${error}`);
+            throw new Error(error);
+        }
+    }
+}
+
+module.exports = new ListingPlansDAO;
