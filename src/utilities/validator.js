db8f07eb5a16d7e3bde799ec32756a3928f88b4f
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2024-03-19 22:17:31 -0300

    feat: Validando campos de entrada ao atualizar o usuário

--- src/utilities/validator.js
+++ src/utilities/validator.js
@@ -1,8 +1,23 @@
 const validator = require("validator");
 
 class Validator {
+
+    // Textos completos ex: Sobre mim onde pode haver números
+    regexFullText = /^[a-zA-ZáàâãéèêíìîóòôõúùûçÁÀÂÃÉÈÊÍÌÎÓÒÔÕÚÙÛÇ\s,.0-9\-]+$/;
+    // Apenas números
+    regexNumber = /^[0-9]+$/;
+
+    validFullText = (text) => {
+        return this.regexFullText.test(text);
+    }
+
     validText = (text) => {
         return validator.isAlpha(text, 'pt-BR', { ignore: 'áàâãéèêíìîóòôõúùûçÁÀÂÃÉÈÊÍÌÎÓÒÔÕÚÙÛÇ ' });
+    }
+
+    validNumber(number) {
+        const stringValue = String(number);
+        return validator.isNumeric(stringValue);
     }
 
     validEmail = (email) => {
@@ -11,15 +26,47 @@ class Validator {
 
     validUser = (user) => {
         if(!this.validEmail(user.getEmail())) {
-            return {error: "Formato do e-mail inválido"};
+            return {error: "Formato do e-mail inválido."};
         }
 
-        if(!this.validText(user.getName())) {
-            return {error: "Formato do nome invalido"};
+        if(!this.validFullText(user.getName())) {
+            return {error: "O campo 'Nome' contém caracteres inválidos."};
         }
 
-        if(!this.validText(user.getLastName())) {
-            return {error: "Formato do sobrenome invalido"};
+        if(!this.validFullText(user.getLastName())) {
+            return {error: "O campo 'Sobrenome' contém caracteres inválidos."};
+        }
+
+        if(!this.validFullText(user.getAboutMe())) {
+            return {error: "O campo 'Sobre mim' contém caracteres inválidos."};
+        }
+
+        if(!this.validFullText(user.getAddress())) {
+            return {error: "O campo 'Endereço' contém caracteres inválidos."};
+        }
+
+        if(!this.validFullText(user.getComplement())) {
+            return {error: "O campo 'Complemento' contém caracteres inválidos."};
+        }
+
+        if(!this.validText(user.getCountry())) {
+            return {error: "O campo 'País' contém caracteres inválidos."};
+        }
+
+        if(!this.validText(user.getState())) {
+            return {error: "O campo 'Estado' contém caracteres inválidos."};
+        }
+
+        if(!this.validText(user.getCity())) {
+            return {error: "O campo 'Cidade' contém caracteres inválidos."};
+        }
+
+        if(!this.validNumber(parseInt(user.getCep()))) {
+            return {error: "Formato do cep invalido."};
+        }
+
+        if(!this.validNumber(parseInt(user.getPhone()))) {
+            return {error: "Formato do telefone invalido."};
         }
 
         return null
